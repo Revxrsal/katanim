bf70f712692883d8285983c0b733a40775651bee
Revxrsal <aloosh.kasasbeh@gmail.com>
2024-08-15 23:19:14 +0300

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 subprojects {
 
     group = "io.github.revxrsal"
-    version = "0.0.0.1"
+    version = "0.0.0.2"
 
     apply(plugin = "java")
     apply(plugin = "com.vanniktech.maven.publish")
